ff6d4c9389a0e0e12d13ce519c2779899305b4f5
Boris Shabanov <b.shabanov@cti.ru>
2013-02-03 21:01:28 +0400

    прикручиваем moodle

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-#source ~/.rvm/scripts/rvm && rvm gemset use moodle-accessibility-theme && sass --update ./sass:./css
+
+clear
+
 echo "Setting shell"
 source ~/.rvm/scripts/rvm && rvm gemset use moodle-accessibility-theme
 
@@ -8,5 +10,6 @@ sass --update ./sass:./temp/css
 
 echo "Contraction CSS"
 csstidy ./temp/css/index.css ./index.css
-
+echo ""
+echo "------"
 echo "Finish"
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+fileschanged -x ./build.sh ./sass/
